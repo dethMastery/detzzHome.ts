144aa422f7a1121f2b79401b67a5c50c7f7734a2
dethMastery <me@suphakit.net>
2023-05-15 15:38:50 +0700

    chore: essential class

---
+++ src/constants/tailwinds.class.ts
@@ -0,0 +1,17 @@
+const TAILWIND_CLASS = {
+  CONTAINER: 'container mx-auto min-h-screen',
+  HEADER_BOX:
+    'w-full h-screen flex flex-row item-center justify-center text-center',
+  LINK: 'underline duration-300 hover:opacity-60 hover:no-underline',
+  HOVER_SCALE: 'duration-300 hover:scale-85 hover:opacity-60',
+  BUTTON:
+    'p-4 rounded-[1rem] w-auto duration-300 hover:scale-85 hover:opacity-60',
+  HEADING: {
+    XXXL: 'text-3xl py-8',
+    XXL: 'text-2xl py-6',
+    XL: 'text-xl py-4',
+    L: 'text-l py-2',
+  },
+}
+
+export default TAILWIND_CLASS

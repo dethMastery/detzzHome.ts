1955b78b2faa64ad6695a7a2c227573dcd71c834
dethMastery <me@suphakit.net>
2023-05-15 16:10:30 +0700

    feat: footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="w-full py-8 text-center bg-gradient-to-t from-[black] from-0% to-[transparent] to-120%">
+      <p>
+        designed & made w/ 🤍 by <a href="https://suphakit.net/">Suphakit P.</a>
+      </p>
+    </div>
+  )
+}
+
+export default Footer

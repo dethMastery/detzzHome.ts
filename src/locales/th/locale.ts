ca7b5a5e833ea3166b4cc7b6401a659ba69e7f46
dethMastery <me@suphakit.net>
2023-05-16 01:58:44 +0700

    chore: localization

--- src/locales/th/locale.ts
+++ src/locales/th/locale.ts
@@ -1,8 +1,9 @@
+import base from './json/base.json'
 import card from './json/card.json'
 import contact from './json/contact.json'
 
 export default function locale() {
-  const mergedLang = {...card, ...contact}
+  const mergedLang = {...base ,...card, ...contact}
 
   return mergedLang
 }

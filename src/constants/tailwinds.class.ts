f6da256ebddd53be5e4b619c1d988ceffffe011c
dethMastery <me@suphakit.net>
2023-05-15 17:03:06 +0700

    chore: tailwinds class

--- src/constants/tailwinds.class.ts
+++ src/constants/tailwinds.class.ts
@@ -1,7 +1,7 @@
 const TAILWIND_CLASS = {
   CONTAINER: 'container mx-auto min-h-screen',
   HEADER_BOX:
-    'w-full h-screen flex flex-row items-center justify-center text-center',
+    'w-full h-screen !bg-center !bg-no-repeat !bg-cover',
   LINK: 'underline duration-300 hover:opacity-60 hover:no-underline',
   HOVER_SCALE: 'duration-300 hover:scale-85 hover:opacity-60',
   BUTTON:
@@ -12,6 +12,7 @@ const TAILWIND_CLASS = {
     XL: 'text-xl py-4',
     L: 'text-l py-2',
   },
+  MIDDLE_FLEX: 'flex flex-col items-center justify-center text-center'
 }
 
 export default TAILWIND_CLASS

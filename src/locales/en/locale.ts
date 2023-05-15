b538150b738d46e0d38ade90b46afd356b97c4ae
dethMastery <me@suphakit.net>
2023-05-16 01:44:56 +0700

    chore: localizatikon

--- src/locales/en/locale.ts
+++ src/locales/en/locale.ts
@@ -1,7 +1,8 @@
+import card from './json/card.json'
 import contact from './json/contact.json'
 
 export default function locale() {
-  const mergedLang = {...contact}
+  const mergedLang = {...card, ...contact}
 
   return mergedLang
 }

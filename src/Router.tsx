3ea1738c65cbdb46f6cb796cd4db0d924201ae38
dethMastery <me@suphakit.net>
2023-05-16 13:11:48 +0700

    chore: route

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import ROUTES from './constants/route'
 import Layout from './pages/_Layout'
 
 import Home from './pages/Home/Index'
+import JoinedEvents from './pages/Events/JoinedEvents'
 import NotFound from './pages/Errors/404'
 
 export const Router = () => {
@@ -11,6 +12,7 @@ export const Router = () => {
       <Layout>
         <Routes>
           <Route path={ROUTES.HOME} element={<Home />} />
+          <Route path={ROUTES.JOINED_EVENTS} element={<JoinedEvents />} />
 
           <Route path="*" element={<NotFound />} />
         </Routes>
--- src/constants/route.ts
+++ src/constants/route.ts
@@ -1,5 +1,6 @@
 const ROUTES = {
-  HOME: '/'
+  HOME: '/',
+  JOINED_EVENTS: '/events'
 }
 
 export default ROUTES

285e8efda5a23d0be09012084fc7c64839ef2018
dethMastery <me@suphakit.net>
2023-05-16 13:41:00 +0700

    chore: parallax custom height

--- src/components/Parallax.tsx
+++ src/components/Parallax.tsx
@@ -1,11 +1,18 @@
 interface Props {
   bg: string
+  height?: string
   children?: string | JSX.Element | JSX.Element[]
 }
 
 const Parallax = (props: Props) => {
   return (
-    <div className='w-full h-[30vh] !bg-fixed !bg-center !bg-cover !bg-no-repeat' style={{background: `url(${props.bg})`}}>
+    <div
+      className="w-full !bg-fixed !bg-center !bg-cover !bg-no-repeat"
+      style={{
+        background: `url(${props.bg})`,
+        height: props.height != undefined ? props.height : `30vh`,
+      }}
+    >
       <div className="w-full h-full backdrop-blur-[5px] bg-overlay">
         {props.children}
       </div>

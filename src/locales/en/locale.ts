10d3873893c8a3b022c707c1fbd2312a6e54029a
dethMastery <me@suphakit.net>
2023-05-16 13:40:35 +0700

    chore: localize setting up

--- src/locales/en/locale.ts
+++ src/locales/en/locale.ts
@@ -3,9 +3,10 @@ import card from './json/card.json'
 import contact from './json/contact.json'
 import nav from './json/nav.json'
 import about from './json/about.json'
+import events from './json/events.json'
 
 export default function locale() {
-  const mergedLang = {...base ,...card, ...contact, ...nav, ...about}
+  const mergedLang = {...base ,...card, ...contact, ...nav, ...about, ...events}
 
   return mergedLang
 }
